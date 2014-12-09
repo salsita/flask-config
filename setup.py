b2f06709016e8b41b6f3c123b6e1ad69ec8d69e7
Jan Killian <jan.killian@gmail.com>
2014-12-09 13:02:20 +0100

    chore: update pypi python version classifiers

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ properties = dict(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'License :: OSI Approved :: MIT License',
